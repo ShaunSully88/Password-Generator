1fc1943ba5ad3201f2e8cb67bb775879a086b8f4
Shaun Sullivan <shaunsully88@gmail.com>
2022-03-11 12:26:28 -0500

    trying to add if statements for each indivudal question..having issues linking answers to correct statement

--- assets/password.js
+++ assets/password.js
@@ -10,30 +10,55 @@ var generatePassword = function () {
   if (pwlength >=8 && pwlength <=128) {
     window.alert("You chose " + pwlength + " characters");
     console.log("Your password will be " + pwlength + " characters long.")
+  }  else {
+    window.alert ("Invalid Entry, please try again");
+    generatePassword();
+      
+  };
 
      var alphaLower = "abcdefghijklmnopqrstuvwxyz";
-    window.prompt ("Would you like Lowercase letters in your password? Click OK for Yes, click Cancel for No");
+    window.prompt ("Would you like lowercase letters in your password? Click OK for Yes, click Cancel for No");
     console.log("alphaLower", alphaLower);
+
+    if (alphaLower = true) {
+       window.alert("You will have lowercase letters in your password.");
+    } else {
+      window.alert("You will have no lowercase letters in your password.");
+    } 
   
 
     var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    window.prompt ("Would you like Uppercase letters in your password? Click OK for Yes, click Cancel for No");
+    window.prompt ("Would you like uppercase letters in your password? Click OK for Yes, click Cancel for No");
     console.log("alphaUpper", alphaUpper)
+
+    if (alphaUpper = true) {
+      window.alert("You will have uppercase letters in your password.");
+    } else {
+      window.alert("You will have no uppercase letters in your password.");
+    }
    
 
     var numeric = "0123456789";
     window.prompt ("Would you like Numbers in your password? Click OK for Yes, click Cancel for No");
     console.log("Numeric", numeric);
+
+    if (numeric = true) {
+      window.alert("You will have numbers in your password");
+    } else {
+      window.alert("You will have no numbers in your password.");
+    }
     
     var symbol = "!@#$%^&*+-";
     window.prompt ("Would you like Symbols in your password? Click OK for Yes, click Cancel for No");
     console.log("symbol", symbol);
-    
-  }  else {
-      window.alert ("Invalid Entry, please try again");
-      generatePassword();
-        
+
+    if (symbol = true)  {
+      window.alert("You will have symbols in your password.");
+    } else {
+      window.alert("You will have no symbols in your password.");
     };
+    
+  
     
  
   

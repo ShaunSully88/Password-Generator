f4e4e60750243efba671cbaf29f3522b1bd1da86
Shaun Sullivan <shaunsully88@gmail.com>
2022-03-12 13:28:39 -0500

    had if statements in wrong part of code, moved them into GeneratePassword function along with loop statement at end. Add return statement, and bingo we've got a random password generator wooogit add .

--- assets/password.js
+++ assets/password.js
@@ -8,15 +8,17 @@ var wantsLowercase;
 var wantsUppercase;
 var wantsNumeric;
 var wantsSymbol;
-var buildPassword = "";
+var createPassword = "";
 var poolBuild = "";
-var createPassword;
+
+
 
 var generateBtn = document.querySelector("#generate");
 var generatePassword = function () {
   
    pwlength = window.prompt ("Please choose a number between 8 and 128.");
   
+  var lengthPass = parseInt(pwlength);
   if(pwlength >=8 && pwlength <=128) {
     window.alert("Your password will be " + pwlength + " characters long.");
     console.log("Your password will be " + pwlength + " characters long.")
@@ -28,34 +30,44 @@ var generatePassword = function () {
      console.log("alphaUpper", wantsUppercase)
 
     wantsNumeric = window.confirm ("Would you like Numbers in your password? Click OK for Yes, click Cancel for No");
-    console.log("Numeric", wantsNumeric);
+    console.log("numeric", wantsNumeric);
  
     wantsSymbol = window.confirm ("Would you like Symbols in your password? Click OK for Yes, click Cancel for No");
     console.log("symbol", wantsSymbol);
 
+    
+
   }  else {
       window.alert ("Invalid Entry, please try again");
       generatePassword();
-}
+  }
+
+  if(wantsLowercase) {
+    createPassword += alphaLower;
+    console.log(createPassword);
+  }
+  if(wantsUppercase) {
+    createPassword += alphaUpper;
+    console.log(createPassword);
+  }  
+  if(wantsNumeric) {
+    createPassword += numeric;
+    console.log(createPassword);
+  }
+  if(wantsSymbol) {
+    createPassword += symbol;
+    console.log(createPassword);
+  } 
+
+  for (var i = 0; i < lengthPass; i++) {
+   poolBuild += createPassword [Math.floor(Math.random() * createPassword.length)];
+  }
+  return poolBuild;
+
 };
 
-createPassword = "";
-  for(var i = 0; i < pwlength;) {
-    poolBuild[pwlength]  
-
-    if(wantsLowercase === true && wantsLowercase <= pwlength) {
-      buildPassword += alphaLower[Math.floor(Math.random() * (alphaLower.length - 1))]
-    }
-    if(wantsUppercase === true && wantsUppercase <= pwlength) {
-      buildPassword += alpaUpper[Math.floor(Math.random () * (alphaUpper - 1))]
-    }  
-    if(wantsNumeric === true && wantsNumeric <= pwlength) {
-      buildPassword += numeric[Math.floor(Math.random () * (numeric -1))]
-    }
-    if(wantsSymbol === true && wantsSymbol <= pwlength) {
-      buildPassword += symbol[Math.floor(Math.random () * (symbol -1))]
-    }  
-  }
+ 
+  
   
 
 
